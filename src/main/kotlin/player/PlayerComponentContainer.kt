987d32068feb18103335acea9f079ad696ab81d6
numq <numq.dev@gmail.com>
2023-03-10 15:00:08 +0300

    Add player component container

---
+++ src/main/kotlin/player/PlayerComponentContainer.kt
@@ -0,0 +1,37 @@
+package player
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import controller.PlayerController
+import java.awt.Component
+
+@Composable
+fun PlayerComponentContainer(
+    modifier: Modifier = Modifier,
+    url: String,
+    component: Component,
+    controller: PlayerController,
+) {
+    LaunchedEffect(url) {
+        if (url.isNotBlank()) controller.load(url)
+    }
+    DisposableEffect(url) {
+        onDispose {
+            controller.dispose()
+        }
+    }
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        PlayerComponent(Modifier.weight(1f), component)
+        DefaultControls(Modifier.fillMaxWidth(), controller)
+    }
+}
