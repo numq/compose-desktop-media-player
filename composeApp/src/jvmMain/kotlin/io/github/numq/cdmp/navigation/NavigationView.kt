24a13af5a74b19c71a65701d0086f811d3bd66bd
numq <numq.dev@gmail.com>
2025-08-29 05:07:33 +0300

    Provide refactoring

--- composeApp/src/jvmMain/kotlin/io/github/numq/cdmp/navigation/NavigationView.kt
+++ composeApp/src/jvmMain/kotlin/io/github/numq/cdmp/navigation/NavigationView.kt
@@ -167,7 +167,7 @@ fun NavigationView(feature: NavigationFeature = koinInject()) {
     exceptions.firstOrNull()?.let { exception ->
         BasicAlertDialog(onDismissRequest = { exceptions.removeFirstOrNull() }, content = {
             Surface(
-                modifier = Modifier.wrapContentWidth().wrapContentHeight(), shape = MaterialTheme.shapes.large
+                modifier = Modifier.wrapContentSize(), shape = MaterialTheme.shapes.large
             ) {
                 Column(
                     modifier = Modifier.padding(16.dp),
